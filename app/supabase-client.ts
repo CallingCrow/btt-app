5463386a0bd9075a7cb4e4a5885bfe1abaf0ec1b
AP <61722850+CallingCrow@users.noreply.github.com>
2026-07-06 20:37:46 -0700

    Update to supabase client's behavior w env vars

--- app/supabase-client.ts
+++ app/supabase-client.ts
@@ -3,4 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 var dbLink = process.env.NEXT_PUBLIC_DB_LINK;
 var apiKey = process.env.NEXT_PUBLIC_DB_API;
 
+if (!dbLink) {
+  throw new Error("Missing NEXT_PUBLIC_DB_LINK");
+}
+
+if (!apiKey) {
+  throw new Error("Missing NEXT_PUBLIC_DB_API");
+}
+
 export const supabase = createClient(dbLink, apiKey);
